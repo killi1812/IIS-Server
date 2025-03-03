c891757f0f64691da13c39bdfed3162d9eb200c9
killi1812 <cvok.fran@gmail.com>
2025-03-03 16:38:12 +0100

    Upload file with form

--- httpserver/upload/upload.go
+++ httpserver/upload/upload.go
@@ -18,29 +18,26 @@ func HandleUploadFile(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 	method := parts[len(parts)-1]
 	zap.S().Infof("Parsing method %s", method)
 
-	// request header
-	uploadFileName := r.Header.Get("FileName")
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		zap.S().Errorf("Can't read file from form err = %s", err)
+		httpio.WriteStandardHTTPResponse(w, http.StatusInternalServerError, nil, err)
+	}
 
-	// log client request
-	zap.S().Debugf("Client request = %+v", r)
-	zap.S().Debugf("Upload file name = %+v", uploadFileName)
-
-	// create temp file
 	tempFile, err := os.CreateTemp(config.TMP_FOLDER, "part*")
 	if err != nil {
 		zap.S().Errorf("Cannot create temp file, err = %v", err)
-		// write response
 		httpio.WriteStandardHTTPResponse(w, http.StatusInternalServerError, nil, err)
 		return
 	}
 
 	// get temp file name
 	tempName := tempFile.Name()
-	zap.S().Debugf("Temp file name = %v", tempName)
+	zap.S().Infof("Temp file name = %v", tempName)
 	defer tempFile.Close()
 
 	// copy to temp file
-	n, err := io.Copy(tempFile, r.Body)
+	n, err := io.Copy(tempFile, file)
 	if err != nil {
 		zap.S().Errorf("Cannot copy content to temp file, err = %v", err)
 
@@ -52,26 +49,7 @@ func HandleUploadFile(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 	zap.S().Debugf("Number of bytes written = %v", n)
 	tempFile.Close()
 
-	// get name of upload file
-	filePath := ""
-
-	if uploadFileName != "" {
-		// set file path in format = folder + name
-		filePath = fmt.Sprintf("%v/%v", config.UPLOAD_FOLDER, uploadFileName)
-	} else {
-		// create temp file in upload folder
-		uploadFile, err := os.CreateTemp(config.UPLOAD_FOLDER, UPLOAD_FILE_PREFIX+"*")
-		if err != nil {
-			zap.S().Errorf("Cannot create temp file, err = %v", err)
-
-			// write response
-			httpio.WriteStandardHTTPResponse(w, http.StatusInternalServerError, nil, err)
-			return
-		}
-
-		filePath = uploadFile.Name()
-		uploadFile.Close()
-	}
+	filePath := fmt.Sprintf("%v/%v", config.UPLOAD_FOLDER, header.Filename)
 	zap.S().Debugf("Upload file path = %v", filePath)
 
 	err = os.Rename(tempName, filePath)
